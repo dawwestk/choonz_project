b2e6b15ea7779c2a59ad4d3c5f00cc5bf6bca5d2
Duncan Wright <1002243w@student.gla.ac.uk>
2020-02-17 20:57:45 +0000

    Fixed broken link

--- choonz/templatetags/choonz_template_tags.py
+++ choonz/templatetags/choonz_template_tags.py
@@ -3,6 +3,6 @@ from choonz.models import Playlist
 
 register = template.Library()
 
-@register.inclusion_tag('choonz/categories.html')
+@register.inclusion_tag('choonz/playlists.html')
 def get_playlist_list(current_playlist=None):
     return {'categories': Playlist.objects.all(), 'current_playlist': current_playlist}
