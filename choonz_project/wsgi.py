783bc8dac48f515800a4beb4fb35f7764e798578
Duncan <duncan@hipstreet.lan>
2020-02-28 22:36:30 +0000

    Updated WSGI

--- choonz_project/wsgi.py
+++ choonz_project/wsgi.py
@@ -1,16 +1,16 @@
-"""
-WSGI config for choonz_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path = '/home/choonz/choonz_project/'
+if path not in sys.path;
+    sys.path.append(path)
+
+os.chdir(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'choonz_project.settings')
 
-application = get_wsgi_application()
+import django
+django.setup()
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
