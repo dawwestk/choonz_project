c38c162cca54c64f4f5f42a360b9e1983616a2d7
Duncan Wright <1002243w@student.gla.ac.uk>
2020-03-13 14:56:38 +0000

    Playlists now added to views in template tag container

--- choonz/templatetags/choonz_template_tags.py
+++ choonz/templatetags/choonz_template_tags.py
@@ -3,6 +3,7 @@ from choonz.models import Tag, Playlist
 
 register = template.Library()
 
+
 @register.inclusion_tag('choonz/tags.html')
 def get_tag_list(playlist=None):
     output = {}
@@ -14,6 +15,7 @@ def get_tag_list(playlist=None):
 
     return output
 
+
 @register.inclusion_tag('choonz/playlist_suggestion.html')
 def get_playlist_list():
     output = {}
@@ -22,10 +24,21 @@ def get_playlist_list():
 
     return output
 
+
 @register.inclusion_tag('choonz/song_search_results.html')
 def get_search_results(results=None):
     output = {}
     search_results = results
     output['search_results'] = search_results
+
+    return output
+
+
+@register.inclusion_tag('choonz/playlist_list_item.html')
+def get_playlist_list_item(playlist, user):
+    output = {'playlist_slug': playlist.slug, 'playlist_name': playlist.name,
+              'playlist_get_average_rating': playlist.get_average_rating,
+              'playlist_get_number_of_ratings': playlist.get_number_of_ratings,
+              'playlist_creator': playlist.creator, 'user': user}
 
     return output
